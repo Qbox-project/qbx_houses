f3a9f5a7fd36eb8f3d930ce66212bfb1d1d31ec1
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2022-10-17 21:30:14 +0200

    fix: utilise job type

--- client/main.lua
+++ client/main.lua
@@ -85,7 +85,7 @@ local function showEntranceHeaderMenu()
                     args = {}
                 }
             }
-            if QBCore.Functions.GetPlayerData().job and QBCore.Functions.GetPlayerData().job.name == 'police' then
+            if QBCore.Functions.GetPlayerData().job and QBCore.Functions.GetPlayerData().job.type == 'leo' then
                 headerMenu[#headerMenu+1] = {
                     header = Lang:t("menu.lock_door_police"),
                     params = {
